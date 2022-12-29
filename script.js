1189a585198ab2beec82a22ff19d565cec718bb1
davidrm1997 <53543168+David010997@users.noreply.github.com>
2022-12-29 05:04:26 +0400

    random generator

---
+++ script.js
@@ -0,0 +1,28 @@
+let interval = false;
+
+function generateRandomNumbers() {
+    let min = document.getElementById('min').value;
+    let max = document.getElementById('max').value;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+    document.getElementById("line").innerHTML = randomNum;
+}
+
+function run() {
+    const line = document.getElementById("line");
+    line.classList.add('line-more');
+    if (!interval) {
+        interval = setInterval(generateRandomNumbers, 70);
+    }
+}
+
+function stop() {
+    clearInterval(interval);
+    if (interval) {
+        document.getElementById("line").innerHTML = `<div class="final-result"><span class="result-num">5562</span><span class="date">${moment().format('ddd MMM DD YYYY HH:mm:ss')}</span></div>`;
+    }
+    interval = false;
+}
+
+
